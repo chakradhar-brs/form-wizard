48b0321013c87202ba467831820b622ef891a7e5
chakradhar-brs <ideal.chakri@gmail.com>
2024-04-09 03:45:36 -0700

    update

--- assets/js/signup-step4.js
+++ assets/js/signup-step4.js
@@ -1,28 +1,19 @@
-/** 
-Author: Build Rise Shine with Nyros (BRS) 
-Created: 2023 
-Library / Component: Step 2 form
-Description: Logic for step 2 form
-(c) Copyright by BRS with Nyros. 
-**/
-
-// Validation for the form 2 [radio buttons and dropdown] for signup-step4.html
-function validate() {
-    let plan = document.forms['myform']['plan'].value
-    let sel_opt = document.forms['myform']['sel_opt'].value
-  
-    if (plan == '') {
-      document.getElementById('error1').innerHTML = 'Please select anyone'
-  
-      return false
-    } else if (plan == 'a' || plan == 'b' || plan == 'c') {
-      document.getElementById('error1').innerHTML = ''
+else if (type == 'radio') {
+  let l = x[i].parentNode.children.length
+  for (let j = 0; j < l; j++) {
+    if (x[i].parentNode.children[j].type == 'radio') {
+      radioButton++
     }
-  
-    if (sel_opt == 'Select an Option') {
-      document.getElementById('error2').innerHTML = 'Please select anyone'
-      return false
-    } else if (sel_opt == 'a' || sel_opt == 'b' || sel_opt == 'c') {
-      document.getElementById('error2').innerHTML = ''
+    if (x[i].parentNode.children[j].checked == true) {
+      radioCheck++
+      x[i].style.outline = '0px'
     }
+  }
+  if (radioButton > 0 && radioCheck == 0) {
+    x[i].focus()
+    x[i].style.outline = '1px solid red'
+    return false
+  } else {
+    radioButton = 0
+    radioCheck = 0
   }
